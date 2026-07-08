f5105fa14378a11e86ff9a1105285afd1300f1c0
Elytridfly <cxj69.23@outlook.com>
2026-07-08 15:43:00 +0800

    init git repo

---
+++ dr.chrome/assets/Topdown Hospital/README.txt
@@ -0,0 +1,35 @@
+📄 README – Top Down Hospital
+
+📦 Content
+
+This pack contains pixel art assets in 16-bit top-down style, including:
+
+- Modular hospital rooms
+- Furniture and medical equipment
+- Spritesheets for integration in game engines
+
+🛠️ Usage
+
+You are free to use these assets in:
+✅ Personal projects
+✅ Commercial projects
+✅ Game jams, prototypes, and educational games
+
+You may:
+Modify, edit, and adapt the assets to fit your needs
+Combine them with your own or third-party assets
+
+❌ Restrictions
+
+You may NOT:
+Redistribute or sell this pack as-is (even for free)
+Claim the assets as your own work
+Include them in another asset pack for resale
+
+❤️ Credit
+
+Credit is not required but always appreciated!
+
+📬 Support
+
+This pack is free. If you enjoy it and want to support more updates, consider donating on my Itch.io page 💖
---
+++ dr.chrome/scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+const SPEED = 90.0
+var last_dir: Vector2 = Vector2.RIGHT
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(_delta: float) -> void:
+	
+	if Input.is_action_just_pressed("interact"):
+		interact()
+	
+	process_movement()
+	process_anim()
+	move_and_slide()
+
+
+
+
+
+# Movement and Anim Stuffs
+
+func process_movement() -> void:
+	var direction := Input.get_vector("left", "right","up","down")
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_dir = direction
+	else:
+		velocity = Vector2.ZERO
+	
+
+
+func process_anim() -> void:
+	if velocity != Vector2.ZERO:
+		play_anim("walking", last_dir)
+	else:
+		play_anim("idle", last_dir)
+	
+
+
+func play_anim(prefix: String, dir: Vector2) -> void:
+	if dir.x != 0:
+		anim.flip_h = dir.x<0
+		anim.play(prefix + "90")
+	elif dir.y < 0:
+		anim.play(prefix + "180")
+	elif dir.y > 0:
+		anim.play(prefix + "0")	
